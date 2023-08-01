4fbcfdac63f944f46eff16aa7515484af53e1dd3
maruf <dev.maruf112@gmail.com>
2023-08-01 07:43:38 +0600

    initial server setup done

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Application, NextFunction, Response, Request } from "express";
+import cors from "cors";
+
+const app: Application = express();
+// cors user
+app.use(cors());
+// parser user
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Application routes
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello World!");
+  // console.log("Hello");
+});
+
+export default app;
