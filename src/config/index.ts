bae4f06917b5add84f1502875eb6c68bd08ed1b6
maruf <dev.maruf112@gmail.com>
2023-08-01 07:50:13 +0600

    initial setup

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,14 +5,4 @@ export default {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
-  default_student_password: process.env.DEFAULT_STUDENT_PASSWORD,
-  default_faculty_password: process.env.DEFAULT_FACULTY_PASSWORD,
-  default_admin_password: process.env.DEFAULT_ADMIN_PASSWORD,
-  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
-  jwt: {
-    token: process.env.JWT_SECRET,
-    token_expires: process.env.JWT_EXPIRES_IN,
-    refresh_token: process.env.JWT_REFRESH_SECRET,
-    refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
-  },
 };
